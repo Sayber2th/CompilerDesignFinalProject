bddd83c139d9bc3101235d1238bdccf507cffaf1
Omkar Kulkarni <kulkarniomkar770@gmail.com>
2026-01-07 19:40:42 +0530

    Define initial list of supported token types

--- Project1/lexer.h
+++ Project1/lexer.h
@@ -3,4 +3,12 @@
 #include <fstream>
 #include <iostream>
 
+enum TokenType 
+{ 
+	Identifier, 
+	Integer, 
+	Semicolon, Comma, 
+	Plus, Minus, Star, ForwardSlash 
+};
+
 std::string readFile(std::string filename);
