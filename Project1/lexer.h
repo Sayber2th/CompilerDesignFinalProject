b0969f1c84d826f7a2871a92043646e35a450437
Omkar Kulkarni <kulkarniomkar770@gmail.com>
2026-01-07 20:02:05 +0530

    Defined an initial lit of supported token types

--- Project1/lexer.h
+++ Project1/lexer.h
@@ -3,4 +3,25 @@
 #include <fstream>
 #include <iostream>
 
+enum TokenType 
+{ 
+	IDENTIFIER, 
+	INTEGER_LITERAL, 
+	SEMICOLON, COMMA, 
+	PLUS, MINUS, STAR, SLASH,
+	UNKNOWN
+};
+
+struct Token
+{
+	TokenType type;
+	std::string value;
+
+	Token(TokenType t, const std::string& v)
+		: type(t)
+		, value(v)
+	{
+	}
+};
+
 std::string readFile(std::string filename);
