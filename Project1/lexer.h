e98835383ed47af4b217ef7d852559a72eff6a5d
Omkar Kulkarni <kulkarniomkar770@gmail.com>
2026-01-07 19:41:32 +0530

    Revert "Define initial list of supported token types"
    This reverts commit bddd83c139d9bc3101235d1238bdccf507cffaf1.

--- Project1/lexer.h
+++ Project1/lexer.h
@@ -3,12 +3,4 @@
 #include <fstream>
 #include <iostream>
 
-enum TokenType 
-{ 
-	Identifier, 
-	Integer, 
-	Semicolon, Comma, 
-	Plus, Minus, Star, ForwardSlash 
-};
-
 std::string readFile(std::string filename);
