f12d12c2ea4fb3d8d4acb9537e0f38f7dae76027
Omkar Kulkarni <kulkarniomkar770@gmail.com>
2026-01-08 11:52:32 +0530

    Minor code cleanup

--- Project1/driver.cpp
+++ Project1/driver.cpp
@@ -2,18 +2,12 @@
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Compiler run!" << std::endl;
-	std::cout << argv[0] << std::endl;
-	std::cout << argv[1] << std::endl;
-
 	if (argc < 2) {
-		std::cerr << "Usage: mycompiler <source-file>\n";
+		std::cerr << "Usage: Project1.exe <source-file-path>";
 		return 1;
 	}
 	std::string file_path = argv[1];
-
 	std::string file_contents = readFile(file_path);
-	std::cout << file_contents << std::endl;
 
 	return 0;
 }
@@ -23,7 +17,7 @@ std::string readFile(std::string file_name)
 	std::ifstream file(file_name);
 
 	if (!file.is_open()) {
-		std::cerr << "Error: could not open file " << file_name << "\n";
+		std::cerr << "Error: Could not open file " << file_name << "\n";
 		exit(1);
 	}
 
